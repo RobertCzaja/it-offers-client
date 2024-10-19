ee563b63c3725f43838e624e44b45f8d5aa11073
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-19 20:55:28 +0200

    Changing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './resource/logo.svg';
+import './resource/App.css';
 
 function App() {
   return (

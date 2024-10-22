1e20619ae270ae86c7a04fb9359f4101b0805bae
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-22 16:54:51 +0200

    add menu

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,18 @@ import "react-toastify/dist/ReactToastify.css";
 import {Route, Routes, useNavigate} from "react-router-dom";
 import {Home} from "./Components/Home";
 import {About} from "./Components/About";
+import {Navbar} from "./Components/Navbar";
 
 function App() {
     return (
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
+        <>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+        </>
+
         /*<>
             <UserProvider>
                 <div>something</div>

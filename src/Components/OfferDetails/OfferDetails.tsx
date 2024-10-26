6d751196862a723e972faa82934f78efcce56fb5
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-26 07:57:47 +0200

    Getting data from mocked axios

--- src/Components/OfferDetails/OfferDetails.tsx
+++ src/Components/OfferDetails/OfferDetails.tsx
@@ -1,18 +1,43 @@
 import {OfferDetailsConnector, OfferDetailsDto} from "./OfferDetailsConnector";
+import HttpRequestMock from 'http-request-mock';
+import axios from "axios";
 
 interface OfferDetailsProps {
     offerId: string
 }
 
+/**
+ * todo why is render twice
+ */
 export const OfferDetails = ({offerId}: OfferDetailsProps) => {
 
-    const details: OfferDetailsDto = OfferDetailsConnector(offerId);
+    const mocker = HttpRequestMock.setup();
+    mocker.mock({
+        url: 'https://www.api.com/some-api',
+        method: 'get',
+        delay: 1000,
+        status: 200,
+        headers: {
+            'content-type': 'application/json',
+            'some-header': 'value',
+        },
+        body: {body: 'something'}
+    });
+
+    const fetch = async () => {
+        return await axios
+            .get('https://www.api.com/some-api')
+            .then(response => {
+                // todo check if it's 200 ok
+                return response.data;
+            });
+    };
+
+    fetch().then(data => {console.log(data);})
 
     return (
         <div>
             <div>OfferDetails {offerId}</div>
-            <div>{details.title}</div>
-            <div>{details.url}</div>
         </div>
     )
 }
--- src/Components/OfferDetails/OfferDetailsConnector.tsx
+++ src/Components/OfferDetails/OfferDetailsConnector.tsx
@@ -1,11 +1,15 @@
 export interface OfferDetailsDto {
+    id: string,
     title: string,
     url: string,
 }
 
-export const OfferDetailsConnector = (offerId: string): OfferDetailsDto => {
-    return {
-        title: "One Identity Consultant z j. angielskim",
-        url: "https://justjoin.it/job-offer/dmtech-polska-c-backend-developer-z-j-angielskim-iam--bydgoszcz-net",
-    };
+export const OfferDetailsConnector = (offerId: string): Promise<OfferDetailsDto> => {
+    return new Promise((resolve) => {
+        resolve({
+            id: offerId,
+            title: "One Identity Consultant z j. angielskim",
+            url: "https://justjoin.it/job-offer/dmtech-polska-c-backend-developer-z-j-angielskim-iam--bydgoszcz-net",
+        });
+    });
 }

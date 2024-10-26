6eb8da8689ce1caef8673750c68f0c2e684cfac4
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-26 15:41:29 +0200

    Add modal to offer details

--- src/Components/OfferDetails/OfferDetails.tsx
+++ src/Components/OfferDetails/OfferDetails.tsx
@@ -1,5 +1,8 @@
 import {FetchOfferDetails, OfferDetailsDto} from "./FetchOfferDetails";
 import {useEffect, useState} from "react";
+import React from 'react';
+import Modal from 'react-modal';
+import offerDetailsCss from "./offerDetailsModal.css";
 
 interface OfferDetailsProps {
     offerId: string
@@ -8,21 +11,53 @@ interface OfferDetailsProps {
 export const OfferDetails = ({offerId}: OfferDetailsProps) => {
 
     const [offerDetails, setOfferDetails] = useState<OfferDetailsDto|null>(null);
+    const [modalIsOpen, setIsOpen] = useState(false);
 
     useEffect(() => {
         FetchOfferDetails(offerId).then(data => setOfferDetails(data));
     }, [offerId]);
 
+    function openModal() {
+        setIsOpen(true);
+        /*todo add implementation*/
+    }
+
+    function afterOpenModal() {
+        /*todo add implementation*/
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+        /*todo add implementation*/
+    }
+
     return (
         <div>
-            <div className='offer-details'>OfferDetails</div>
-            {offerDetails ? <div className='content'>
-                <ul>
-                    <li>{offerDetails.id}</li>
-                    <li>{offerDetails.title}</li>
-                    <li>{offerDetails.url}</li>
-                </ul>
-            </div> : null}
+            <button onClick={openModal}>Offer Details</button>
+            <Modal
+                isOpen={modalIsOpen}
+                onAfterOpen={afterOpenModal}
+                onRequestClose={closeModal}
+                style={offerDetailsCss}
+                contentLabel="Offer details"
+            >
+                <h2>Offer details</h2>
+                <button onClick={closeModal}>close</button>
+                <div>Modal details</div>
+            </Modal>
         </div>
-    )
+    );
+
+    // return (
+    //     <div>
+    //         <div className='offer-details'>OfferDetails</div>
+    //         {offerDetails ? <div className='content'>
+    //             <ul>
+    //                 <li>{offerDetails.id}</li>
+    //                 <li>{offerDetails.title}</li>
+    //                 <li>{offerDetails.url}</li>
+    //             </ul>
+    //         </div> : null}
+    //     </div>
+    // )
 }

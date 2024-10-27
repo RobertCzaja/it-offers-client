e1d05191cff416698103ba4dfea8bd8bf3ab7d3f
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-27 08:23:41 +0100

    Add format date

--- src/Components/OfferList/OfferList.tsx
+++ src/Components/OfferList/OfferList.tsx
@@ -6,14 +6,21 @@ import './OfferList.css';
 
 export const OfferList = () => {
     const rows: GridRowsProp = MockedData.offers();
+    const dateFormatter = new Intl.DateTimeFormat("pl-PL");
 
     const columns: GridColDef[] = [
-        { field: 'technology', headerName: 'technology', width: 100 },
+        { field: 'technology', headerName: 'technology', width: 100, align: "center"},
         { field: 'title', headerName: 'Job', width: 250 },
         { field: 'salaryFrom', headerName: 'From', type: 'number', width: 75 },
         { field: 'salaryTo', headerName: 'To', type: 'number', width: 75 },
         { field: 'salaryCurrency', headerName: 'Currency', width: 75 },
-        { field: 'publishedAt', headerName: 'Published', width: 100 }, //todo check it there is not special type for date
+        {
+            field: 'publishedAt',
+            headerName: 'Published',
+            type: 'date',
+            width: 100,
+            valueFormatter: (value) => dateFormatter.format(value),
+        },
         { field: 'seniority', headerName: 'Level', width: 100 },
         { field: 'companyName', headerName: 'Company', width: 100 },
         { field: 'companyCity', headerName: 'City', width: 100 },
@@ -30,10 +37,8 @@ export const OfferList = () => {
     ];
 
     return (
-        <>
-            <div className='offer-list'>
-                <DataGrid rows={rows} columns={columns} />
-            </div>
-        </>
+        <div className='offer-list'>
+            <DataGrid rows={rows} columns={columns} />
+        </div>
     );
 }

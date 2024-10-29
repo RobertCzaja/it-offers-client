bcab69a8618440a448c87d4e719a0de63e95607e
RobertCzaja <robertczaja.dev@gmail.com>
2024-10-29 21:14:40 +0100

    Cleaning

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import {toast, ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import {Route, Routes, useNavigate} from "react-router-dom";
 import {Home} from "./Components/Home";
-import {About} from "./Components/About";
 import {Navbar} from "./Components/Navbar";
 import LoginPage from "./Pages/LoginPages/LoginPage";
 import LoginForm from "./Components/LoginForm/LoginForm";
@@ -15,9 +14,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 function App() {
-
-    //const [theme, setTheme] = useState('light');
-
     return (
         <>
             <UserProvider>
@@ -25,7 +21,6 @@ function App() {
                     {/* <Navbar theme={theme} setTheme={setTheme}/> */}
                     <Routes>
                         <Route path='/' element={<Home />} />
-                        <Route path='/about' element={<About />} />
                         <Route path='/login' element={<LoginPage />} />
                         <Route path='/new-login' element={<LoginForm />} />
                     </Routes>
--- src/Components/OfferDetails/OfferDetailsModal.tsx
+++ src/Components/OfferDetails/OfferDetailsModal.tsx
@@ -91,19 +91,19 @@ export const OfferDetailsModal = ({open, handleClose, offerId}: OfferDetailsModa
             }}>
                 <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                     <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                        <Tab label="Item One" {...a11yProps(0)} />
-                        <Tab label="Item Two" {...a11yProps(1)} />
-                        <Tab label="Item Three" {...a11yProps(2)} />
+                        <Tab label="Details" {...a11yProps(0)} />
+                        <Tab label="Categories" {...a11yProps(1)} />
+                        <Tab label="Salary" {...a11yProps(2)} />
                     </Tabs>
                 </Box>
                 <CustomTabPanel value={value} index={0}>
-                    Item One
+                    Details
                 </CustomTabPanel>
                 <CustomTabPanel value={value} index={1}>
-                    Item Two
+                    Categories
                 </CustomTabPanel>
                 <CustomTabPanel value={value} index={2}>
-                    Item Three
+                    Salary
                 </CustomTabPanel>
 
             </DialogContent>

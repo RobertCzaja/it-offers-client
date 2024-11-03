65c3f0e9e20d6a2668a94f6cb6ed2d556280546c
RobertCzaja <robertczaja.dev@gmail.com>
2024-11-03 07:25:14 +0100

    Add login credentials validation

--- src/Components/Authorization/LoginForm.tsx
+++ src/Components/Authorization/LoginForm.tsx
@@ -1,57 +1,48 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {
-    Box,
     Grid2,
     TextField
 } from "@mui/material";
 import Button from "@mui/material/Button";
 import {FetchToken} from "./FetchToken";
 import {Token} from "./AuthorizationTypes";
+import {SubmitHandler, useForm} from "react-hook-form";
+
+type LoginFormValues = {
+    email: string
+    password: string
+}
 
 export const LoginForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState:{errors},
+    } = useForm<LoginFormValues>({defaultValues: {email: "", password: "",}});
 
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-
-    const handleSubmit = (event: React.SyntheticEvent) => {
-        console.log(email, password); // todo to remove
-        FetchToken({email: email, password: password}).then((token: Token) => console.log(token.value));
+    const onSubmit: SubmitHandler<LoginFormValues> = (data: LoginFormValues) => {
+        FetchToken(data).then((token: Token) => console.log(token.value) /* todo remove*/);
         // todo set it in global context
     }
 
-    // todo add email validation; password some simple length validation
-    // todo use form react package
-
     return <>
-        <Box component="form" noValidate sx={{mt: 3}}>
-            <Grid2 container spacing={2}>
-                <Grid2>
-                   <TextField
-                       name="email"
-                       required
-                       value={email}
-                       onChange={e => setEmail(e.target.value)}
-                       fullWidth
-                       id="email"
-                       label="Email"
-                       autoFocus
-                       size="small"
-                   />
-                   <TextField
-                       name="password"
-                       required
-                       value={password}
-                       onChange={e => setPassword(e.target.value)}
-                       fullWidth
-                       id="password"
-                       label="Password"
-                       autoFocus
-                       size="small"
-                   />
-
-                    <Button variant="outlined" onClick={handleSubmit}>Login</Button>
-                </Grid2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid2 container spacing={1} sx={{mt: 1, ml: 2}}>
+                <TextField
+                    type="email"
+                    size="small"
+                    label="Email"
+                    {...register("email")}
+                />
+                <TextField
+                    type="password"
+                    size="small"
+                    label="Password"
+                    slotProps={{ htmlInput: {minLength: 5, maxLength: 30} }}
+                    {...register("password")}
+                />
+                <Button type="submit" variant="contained">Login</Button>
             </Grid2>
-        </Box>
+        </form>
     </>
 }

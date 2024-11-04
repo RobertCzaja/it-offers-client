6c9831b2356839cba12de8357f4c21eca4f58085
RobertCzaja <robertczaja.dev@gmail.com>
2024-11-04 19:12:33 +0100

    Add also Snackbar on successfull login

--- src/Components/Authorization/LoginForm.tsx
+++ src/Components/Authorization/LoginForm.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {
+    Alert,
     Grid2, Snackbar,
     TextField
 } from "@mui/material";
@@ -8,6 +9,8 @@ import {Token} from "./AuthorizationTypes";
 import {SubmitHandler, useForm} from "react-hook-form";
 import {LoadingButton} from "@mui/lab";
 import {saveToken} from "./TokenRepository";
+import {AxiosError} from "axios";
+import {AlertColor} from "@mui/material/Alert/Alert";
 
 type LoginFormValues = {
     email: string
@@ -15,7 +18,9 @@ type LoginFormValues = {
 }
 
 export const LoginForm = () => {
-    const [errorSnackbar, setErrorSnackbar] = useState(false);
+    const [snackbar, setAuthSnackbar] = useState(false);
+    const [authResultMessage, setAuthResultMessage] = useState<string>('');
+    const [authMessageSeverity, setAuthMessageSeverity] = useState<AlertColor>('success');
     const [loading, setLoading] = useState<boolean>(false);
     const {
         register,
@@ -24,7 +29,7 @@ export const LoginForm = () => {
     } = useForm<LoginFormValues>({defaultValues: {email: "", password: "",}});
 
     const handleCloseErrorSnackbar = () => {
-        setErrorSnackbar(false);
+        setAuthSnackbar(false);
     };
 
     const onSubmit: SubmitHandler<LoginFormValues> = (data: LoginFormValues) => {
@@ -32,15 +37,29 @@ export const LoginForm = () => {
         FetchToken(data).then((token: Token) => {
             saveToken(token.value);
             setLoading(false);
-        }).catch(() => {
+            setAuthResultMessage('Successfully logged');
+            setAuthSnackbar(true);
+            setAuthMessageSeverity('success');
+        }).catch((errorResponse: AxiosError) => {
+            switch (errorResponse.status) {
+                case 401:
+                    setAuthResultMessage('Invalid Credentials');
+                    break;
+                default:
+                    setAuthResultMessage('Unknown error');
+                    break;
+            }
             setLoading(false);
-            setErrorSnackbar(true);
+            setAuthSnackbar(true);
+            setAuthMessageSeverity('error');
         });
     }
 
     return <>
         <form onSubmit={handleSubmit(onSubmit)}>
             <Grid2 container spacing={1} sx={{mt: 1, ml: 2}}>
+                {/*todo better display error in both fields*/}
+                {/*todo on api add more precise cors origin*/}
                 <TextField
                     required
                     type="email"
@@ -65,11 +84,8 @@ export const LoginForm = () => {
                 </LoadingButton>
             </Grid2>
         </form>
-        <Snackbar
-            open={errorSnackbar}
-            autoHideDuration={6000}
-            onClose={handleCloseErrorSnackbar}
-            message="Auth error"
-        />
+        <Snackbar open={snackbar} autoHideDuration={2000} onClose={handleCloseErrorSnackbar}>
+            <Alert severity={authMessageSeverity}>{authResultMessage}</Alert>
+        </Snackbar>
     </>
 }
